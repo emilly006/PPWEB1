887a7defc028a175b04459be8914e348c6e55b82
mariana <mvs38@aluno.ifal.edu.br>
2025-07-02 10:44:43 -0300

    update
    02/07/2024

--- PLAYLIST/config/database.js
+++ PLAYLIST/config/database.js
@@ -1,17 +1,37 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+  import { Sequelize } from 'sequelize';
+  import dotenv from 'dotenv';
 
-dotenv.config(); // Carrega as variáveis de ambiente
+  dotenv.config(); // Carrega as variáveis de ambiente
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT
-  }
-);
+  const env = process.env.NODE_ENV || 'development';
+  const config = {
+    development: {
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+    },
+    test: {
+      database: 'playlist_test',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+      logging: false,
+    },
+    production: {
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+    },
+  };
 
-export default sequelize;
+  const sequelize = new Sequelize(config[env]);
+
+  export default sequelize;
