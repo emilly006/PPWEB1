5c97a0b15b2a0898c6c1e3b92e81210932faba45
mariana <mvs38@aluno.ifal.edu.br>
2025-07-09 10:52:00 -0300

    updte
    updte

--- PLAYLIST/tests/setup.js
+++ PLAYLIST/tests/setup.js
@@ -1,21 +1,21 @@
-import * as models from '../models/Index.js';
-import sequelize from '../config/database.js';
+import * as models from '../models/index.js';
+        import sequelize from '../config/database.js';
 
-const db = { ...models }; // Copia os modelos diretamente
+        const db = { ...models }; // Copia os modelos diretamente
 
-// Sincroniza o banco antes dos testes
-before(async () => {
-  await sequelize.sync({ force: true }); // Recria as tabelas
-});
+        // Sincroniza o banco antes dos testes
+        before(async () => {
+          await sequelize.sync({ force: true }); // Recria as tabelas
+        });
 
-// Limpa todas as tabelas após cada teste
-afterEach(async () => {
-  await sequelize.truncate({ cascade: true, restartIdentity: true });
-});
+        // Limpa todas as tabelas após cada teste
+        afterEach(async () => {
+          await sequelize.truncate({ cascade: true, restartIdentity: true });
+        });
 
-// Fecha a conexão após todos os testes
-after(async () => {
-  await sequelize.close();
-});
+        // Fecha a conexão após todos os testes
+        after(async () => {
+          await sequelize.close();
+        });
 
-export { sequelize, db };
+        export { sequelize, db };
